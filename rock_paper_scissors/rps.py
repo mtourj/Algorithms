3746a37f18f3d0881ae81b9f010cca5aaa6ce5ba
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-03-19 22:36:01 -0700

    Rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python
 
+# Given n, return all possible permutations of n plays
+
+# Example:
+'''
+1.
+[['rock'], ['paper'], ['scissors']]
+
+2.[['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'],
+  ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'],
+  ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+
+'''
+
 import sys
 
+
+cache = {
+  0: [[]],
+  1: [['rock'], ['paper'], ['scissors']]
+}
+
 def rock_paper_scissors(n):
-  pass 
+    plays = []
+
+    if n in cache:
+        return cache[n]
+    else:
+        for play in rock_paper_scissors(n - 1):
+            for valid_play in cache[1]:
+                plays.append(play + valid_play)
+
+    cache[n] = plays
+
+    return cache[n]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
